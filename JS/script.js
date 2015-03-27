4f689526fd6508f9c13ca26383e0b82e84cc9eb1
Ashley Whitley <paintrevelations@gmail.com>
2015-03-27 16:06:57 -0700

    prompt
    prompt needed for assignment

--- JS/script.js
+++ JS/script.js
@@ -5,3 +5,21 @@ March 26, 2015
 SDI Section 01
  */
 
+//Variables:
+
+var request = prompt("Do you want to know who the Florida Lottery winner is?");
+
+
+//Functions:
+
+function Verification(answer) {
+    prompt("You said: " + answer + ". If you want to view the winning numbers please inspect element to see the results on the console.");
+}
+
+
+//Main Code
+
+Verification(request);
+
+
+
