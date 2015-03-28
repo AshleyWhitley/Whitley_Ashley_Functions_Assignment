ccc603b86acba2d5cbc01b01a34dbe8aca9b7a34
Ashley Whitley <paintrevelations@gmail.com>
2015-03-27 18:45:55 -0700

    Finished!
    Was able to finish the code and it is all working.

--- JS/script.js
+++ JS/script.js
@@ -29,16 +29,17 @@ function Verification(answer) {  //argument
 }
 
 
+
 //Function 2:
 
 function lotto(min, max, par) { //parameter of 6 total answers that are random at once
 
-    var rand = [];
+    var rand = []; //inorder to have 6 seperate numbers it has to be an array code
 
-    for (var i = 0; i < par; i++){
+    for (var i = 0; i < par; i++){ //this declares between which numbers those 6 numbers can be.
         var numbers = Math.round(Math.random() * (max - min) + min); //math.random allows completely random numbers every  and Math,round allows for whole numbers and no decimals
 
-        rand[i] = numbers;
+        rand[i] = numbers; // this connects the variable to the array so it can be returned correctly and output correctly
     }
 
     return rand;
@@ -47,10 +48,10 @@ function lotto(min, max, par) { //parameter of 6 total answers that are random a
 //Main Code 1
 
 request = Verification(request);  //value sent to the variable to produce the function
-console.log("Thank you for responding with" + " "+ request + ". Below are the winning numbers!");
+console.log("Thank you for responding with" + " "+ request + ". Below are the winning numbers!"); //string is printed to console for the user to see.
 
 //Main Code 2
 
 rand = lotto(1, 53, 6);
-console.log(rand);
+console.log("The winning numbers are..." + " "+ rand + ". If these are your numbers please call the number on the back of your ticket!");
 
